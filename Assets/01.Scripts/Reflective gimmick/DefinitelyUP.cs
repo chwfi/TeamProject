cbdc5c102109b534ee536062d4e597fb3d8c421b
mingyo0125 <wjdalsry125@gmail.com>
2023-11-05 01:06:48 +0900

    무적권위로만감근데이거뭐상속만받으면끝이라서그냥개날먹이네ㅋㅋ앙개꿀

---
+++ Assets/01.Scripts/Reflective gimmick/DefinitelyUP.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefinitelyUP : Reflective
+{
+    public override void SetDataModify(ReflectData inData)
+    {
+        OnShootRaycast(inData, Vector3.up); // 무조건 위로만
+    }
+
+    public override void OnHandleReflected()
+    {
+        base.OnHandleReflected();
+
+    }
+    public override void UnHandleReflected()
+    {
+        base.UnHandleReflected();
+    }
+}
