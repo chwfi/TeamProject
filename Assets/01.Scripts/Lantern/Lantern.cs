e69b09de1df5e381fa4c01fe50b41093698c75cc
Chwfi <98933635+chwfi@users.noreply.github.com>
2023-11-05 22:20:12 +0900

    일단 안따라오는 버그 고침

--- Assets/01.Scripts/Lantern/Lantern.cs
+++ Assets/01.Scripts/Lantern/Lantern.cs
@@ -1,49 +1,115 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Net.NetworkInformation;
+using System.Xml.Linq;
 using UnityEngine;
 using static Define.Define;
-using static UnityEngine.Rendering.DebugUI;
 
 [RequireComponent(typeof(LineRenderer))]
 public class Lantern : MonoBehaviour
 {
-    [SerializeField] private Transform _firePosition;
+    [SerializeField] private InputReader _inputReader;
 
     public Color defaultColor;
     private LineRenderer lb;
 
+    private ReflectData myReflectData;
+
+    private Reflective reflectObject = null;
+
+    private ReflectState _currentState = ReflectState.NULL;
+
+    public ReflectState CurrentState
+    {
+        get { return _currentState; }
+        set
+        {
+            if (_currentState == value) return;
+
+            _currentState = value;
+        }
+    }
+
     private void Awake()
     {
+        myReflectData.color = defaultColor;
+
         lb = (LineRenderer)GetComponent("LineRenderer");
-        lb.enabled = false;
         lb.positionCount = 2;
         lb.material.color = defaultColor;
+
+        _inputReader.OnStartFireEvent += OnStartShootLight;
+        _inputReader.OnUpdateFireEvent += OnShootLight;
+        _inputReader.OnEndFireEvent += OnEndShootLight;
     }
 
-    public void OnShootLight(bool value) //연결할 함수
+    private void OnStartShootLight()
     {
-        lb.enabled = value;
-        RaycastWithReflection(_firePosition.position, transform.forward);
+        lb.enabled = true;
+    }
+    private void OnEndShootLight()
+    {
+        lb.enabled = false;
     }
 
-    private void RaycastWithReflection(Vector3 origin, Vector3 direction)
+    private void OnShootLight()
     {
-        lb.SetPosition(0, origin);
+        Debug.Log("슈웃");
+        StartShootLight(transform.position, transform.forward);
+    }
 
+    public void StartShootLight(Vector3 origin, Vector3 direction)
+    {
+        myReflectData.hitPos = origin;
+        myReflectData.direction = direction;
+
+        DataModify(myReflectData);
+    }
+
+    public void DataModify(ReflectData reflectData)
+    {
+        lb.SetPosition(0, reflectData.hitPos);
         RaycastHit hit;
-        if (Physics.Raycast(origin, direction, out hit, 1000, ReflectionLayer))
+
+        if (Physics.Raycast(reflectData.hitPos, reflectData.direction, out hit, 1000, ReflectionLayer))
         {
-            Debug.Log("으악");
+            myReflectData.hitPos = hit.point;
+            myReflectData.direction = reflectData.direction;
+            myReflectData.normal = hit.normal;
+
             lb.SetPosition(1, hit.point);
 
-            if (hit.collider.TryGetComponent<IReflectable>(out var reflectableObject))
+            if (hit.collider.TryGetComponent<Reflective>(out var reflectable)) //반사 오브젝트라면
             {
-                //reflectableObject?.OnHandleReflected(hit.point, direction, hit.normal, defaultColor);
+                ChangedReflectObject(reflectable);
+
+                reflectable?.OnReflectTypeChanged(ReflectState.OnReflect);
+                reflectable?.SetDataModify(myReflectData);
             }
         }
         else
         {
-            lb.SetPosition(1, direction * 100);
+            if (reflectObject != null)
+            {
+                reflectObject?.OnReflectTypeChanged(ReflectState.UnReflect);
+                reflectObject = null;
+            }
+
+            lb.SetPosition(1, reflectData.direction * 1000);
         }
+    }
+
+    private void ChangedReflectObject(Reflective reflectable)
+    {
+        if (reflectObject == reflectable) return;
+        reflectObject = reflectable;
+    }
+
+    private void OnDestroy()
+    {
+        _inputReader.OnStartFireEvent -= OnShootLight;
+        _inputReader.OnUpdateFireEvent -= OnShootLight;
+        _inputReader.OnEndFireEvent -= OnShootLight;
     }
 }
