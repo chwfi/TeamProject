09218fbf59490294e440751df635ab1e99e44b18
mingyo0125 <wjdalsry125@gmail.com>
2023-11-08 18:28:46 +0900

    ColorDoor수정 및 DoorOpenTrigger추가

---
+++ Assets/01.Scripts/Level/DoorOpenTrigger.cs
@@ -0,0 +1,23 @@
+using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpenTrigger : MonoBehaviour
+{
+    [SerializeField, Tooltip("목표 색")]
+    private Color targetColor;
+
+    [SerializeField]
+    private ColorDoor linkedDoor; // 열어줄 문
+
+    public void ColorMatch(Color inputColor) // 다른 함수에서 실행하여 비교 함
+    {
+        if (ColorSystem.CompareColor(inputColor, targetColor))
+        {
+            linkedDoor.OpenDoor(); // 같은 색이라면 문 염
+        }
+    }
+
+
+}
