2ad826388b640deb24a4fa6bc3e7915ff314a17e
Chwfi <98933635+chwfi@users.noreply.github.com>
2023-11-09 23:32:04 +0900

    체인
    체인

--- Assets/01.Scripts/Level/DoorOpenTrigger.cs
+++ Assets/01.Scripts/Level/DoorOpenTrigger.cs
@@ -1,6 +1,7 @@
 using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class DoorOpenTrigger : MonoBehaviour
@@ -11,13 +12,25 @@ public class DoorOpenTrigger : MonoBehaviour
     [SerializeField]
     private ColorDoor linkedDoor; // 열어줄 문
 
+    public List<Rigidbody> _rigids;
+
     bool _isOpend = false;
+
+    private void Awake()
+    {
+        _rigids.AddRange(GetComponentsInChildren<Rigidbody>());
+    }
 
     public void ColorMatch(Color inputColor) // 다른 함수에서 실행하여 비교 함
     {
         if (ColorSystem.CompareColor(inputColor, targetColor) && !_isOpend)
         {
             linkedDoor.OpenDoor(); // 같은 색이라면 문 염
+            _rigids.ForEach(p =>
+            {
+                p.useGravity = true;
+                Destroy(p.gameObject, 2f);
+            });
             _isOpend = true;
         }
     }
