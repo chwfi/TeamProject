b99a89f8b2e84b68b3e25b86f820ae75d05f97c1
Chwfi <98933635+chwfi@users.noreply.github.com>
2023-11-11 23:24:25 +0900

    파티클 다운로드
    파티클 다운로드

--- Assets/01.Scripts/Level/Door.cs
+++ Assets/01.Scripts/Level/Door.cs
@@ -5,17 +5,21 @@ using UnityEngine.AddressableAssets;
     
 public class ColorDoor : MonoBehaviour
 {
-    Animator _animator;
+    private Animator _animator;
+    private CameraShake _cameraShake;
 
     private readonly int openDoorHash = Animator.StringToHash("OpenDoorTrigger");
 
     private void Awake()
     {
         _animator = GetComponent<Animator>();
+        _cameraShake = FindObjectOfType<CameraShake>();
     }
 
     public void OpenDoor()
     {
+        Debug.Log("으아아아아아!!!");
         _animator.SetTrigger(openDoorHash);
+        _cameraShake.Shake(4.75f);
     }
 }
---
+++ Assets/01.Scripts/Player/LanternShaking.cs
@@ -0,0 +1,28 @@
+using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.ShaderGraph.Internal;
+using UnityEngine;
+
+public class LanternShaking : MonoBehaviour
+{
+    [SerializeField]
+    private float cameraShakeSensitive;
+
+    Animator _animator;
+
+    bool isMoving => GameManager.Instance.Player.CanMove && GameManager.Instance.Player.TargetSpeed > 0;
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (!TutorialManager.Instance.IsActive) //튜토리얼 팝업이 뜬 상태가 아니라면
+            _animator.SetBool("isMoving", isMoving);
+        else
+            _animator.SetBool("isMoving", false);
+    }
+}
