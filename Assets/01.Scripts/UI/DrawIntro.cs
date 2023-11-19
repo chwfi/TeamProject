70f31222931d8825b3583f9bb7fb91c8157cf3ff
leewnstj <ij35492581@gmail.com>
2023-11-19 16:17:30 +0900

    인트로 하는중

--- Assets/01.Scripts/UI/DrawIntro.cs
+++ Assets/01.Scripts/UI/DrawIntro.cs
@@ -7,27 +7,18 @@ using UnityEngine.UI;
 using TMPro;
 
 [Serializable]
-public class OutputText
+public class OutputContent
 {
     [TextArea] public string text;
+    public Sprite sprite;
     public float waitTime;
     public float fadeTime; //알파 조정 시간
     public float duration;   //지속 시간
 }
 
-[Serializable]
-public class OutputSprite
-{
-    public Sprite sprite;
-    public float waitTime;
-    public float fadeTime;
-    public float duration;
-}
-
 public class DrawIntro : MonoBehaviour
 {
-    public List<OutputText> TextList;
-    public List<OutputSprite> SpriteList;
+    public List<OutputContent> DrawList;
     
     private SpriteRenderer _sprite;
     private TextMeshProUGUI _text;
@@ -40,28 +31,23 @@ public class DrawIntro : MonoBehaviour
 
     private void Start()
     {
-        for (int i = 0; i < TextList.Count; i++)
-        {
-            _sprite.sprite = SpriteList[i].sprite;
-            _text.text = TextList[i].text;
-            
-            StartCoroutine(TextDraw(i, TextList[i].waitTime, TextList[i].duration, TextList[i].fadeTime));
-            StartCoroutine(SpriteDraw(i, SpriteList[i].waitTime, SpriteList[i].duration, SpriteList[i].fadeTime));
-        }
+        StartCoroutine(TextDraw());
     }
 
-    private IEnumerator TextDraw(int i, float waitTIme, float duration, float fadeTime)
+    private IEnumerator TextDraw()
     {
-        yield return new WaitForSeconds(waitTIme);
-        _text.DOFade(1, fadeTime);
-        yield return new WaitForSeconds(duration);
-        _text.DOFade(0, fadeTime);
-    }
-    private IEnumerator SpriteDraw(int i, float waitTIme, float duration, float fadeTime)
-    {
-        yield return new WaitForSeconds(waitTIme);
-        _sprite.DOFade(1, fadeTime);
-        yield return new WaitForSeconds(duration);
-        _sprite.DOFade(0, fadeTime);
+        for (int i = 0; i < DrawList.Count; i++)
+        {
+            _sprite.sprite = DrawList[i].sprite;
+            _text.text = DrawList[i].text;
+
+            yield return new WaitForSeconds(DrawList[i].waitTime);
+            _text.DOFade(1, DrawList[i].fadeTime);
+            _sprite.DOFade(1, DrawList[i].fadeTime);
+            yield return new WaitForSeconds(DrawList[i].duration);
+            _text.DOFade(0, DrawList[i].fadeTime);
+            _sprite.DOFade(0, DrawList[i].fadeTime);
+            yield return new WaitForSeconds(DrawList[i].fadeTime);
+        }
     }
 }
